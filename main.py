c115097e1d5a3d39867499d5955e545f18b43f21
mosescode1 <emosescode@gmail.com>
2024-02-03 16:43:51 +0100

    learnning using turle graphics

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+my_turtle = Turtle()
+my_screen = Screen()
+
+my_turtle.shape('turtle')
+my_turtle.color('coral')
+my_turtle.forward(50)
+my_turtle.right(90)
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(50)
+my_turtle.right(90)
+my_turtle.forward(100)
+
+
+my_screen.exitonclick()
